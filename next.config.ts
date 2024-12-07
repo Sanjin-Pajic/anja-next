c65d12d86686da1faef33dae95d19edd9f3af347
Sanjin Pajic <pajicsanjo@gmail.com>
2024-12-07 09:43:33 +0100

    chore(package.json): Configure Commitizen to ease the Commit process
       - Installed and configured Commitizen for easier Conventional
         Commits-style commiting.

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ import type { NextConfig } from "next";
  * customize build processes, enable optimizations, configure routing, env variables, etc.
  * 
  * This file should serve as the foundation for improving app performance, and 
- * configuring environment-specific settings for all types of environments.
+ * configuring environment-specific settings for the types of environments.
  * However at the current moment we require no adjustments to this logic.
  * 
  * @example
@@ -19,7 +19,7 @@ import type { NextConfig } from "next";
  * @see https://nextjs.org/docs/api-reference/next.config.js
  */
 const nextConfig: NextConfig = {
-  // Configuration settings will be placed here as needed
+  // 
 };
 
 export default nextConfig;
