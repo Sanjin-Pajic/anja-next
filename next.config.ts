b23ce6524cddf997119f49c8bef623b5c040287b
Sanjin Pajic <pajicsanjo@gmail.com>
2024-12-07 09:55:24 +0100

    style(global): adjust all project files for new formatting rules
    - Ran Prettier across the whole project structure to make sure new code guidelines are applied
    everywhere

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 /**
  * The main Next.js configuration file.
- * 
- * Can be used to finetune settings which affect how the application is built and run. We can 
+ *
+ * Can be used to finetune settings which affect how the application is built and run. We can
  * customize build processes, enable optimizations, configure routing, env variables, etc.
- * 
- * This file should serve as the foundation for improving app performance, and 
+ *
+ * This file should serve as the foundation for improving app performance, and
  * configuring environment-specific settings for the types of environments.
  * However at the current moment we require no adjustments to this logic.
- * 
+ *
  * @example
  * // ex of enabling Webpack 5 for builds:
  * {
  *   webpack5: true
  * }
- * 
+ *
  * @see https://nextjs.org/docs/api-reference/next.config.js
  */
 const nextConfig: NextConfig = {
-  // 
-};
+    //
+}
 
-export default nextConfig;
+export default nextConfig
